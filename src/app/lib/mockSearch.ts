2f3ef50bed6e21c6a18339187780d43015cc7475
Gary Clark <gclark04432@gmail.com>
2025-06-19 13:30:11 -0700

    Removes unrequired mockSearch function

--- src/app/lib/mockSearch.ts
+++
@@ -1,8 +0,0 @@
-import { Player } from '@/app/types/Player';
-
-export const mockSearch = (name: string): Player => ({
-  id: Date.now(),
-  name,
-  team: 'Mock Team',
-  points: Math.floor(Math.random() * 50),
-});
